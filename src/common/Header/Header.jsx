9688b077816a12ed0247b65ad3b2735eba2beac0
jluisferrer <jluisferrermartinez@gmail.com>
2024-04-04 19:12:01 +0200

    feat:login/logout working

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,21 +1,39 @@
-import { CLink } from "../CLink/CLink"
 import "./Header.css"
+import { CLink } from "../CLink/CLink"
+import { useSelector, useDispatch } from "react-redux"
+import { userData, logout } from "../../app/slices/userSlice"
+import { useEffect } from "react"
+
 
 export const Header = () => {
+    //instancia de conexion a modo lectura
+    const rdxUser = useSelector(userData)
+    //instancia de conexion a modo escritura
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        console.log(rdxUser, " credenciales pasaporte");
+    }, [rdxUser]);
     return (
         <div className="headerDesign">
-            <CLink
-                path="/"
-                title="Home"
-            />
-            <CLink
-                path="/login"
-                title="Login"
-            />
-            <CLink
-                path="/register"
-                title="Register"
-            />
+            <CLink path="/" title="Home" />
+            {rdxUser?.credentials?.token ? (
+                <div className="navigatorDesign">
+                    <CLink path="/profile" title={rdxUser?.credentials?.user?.userName} />
+                    <div
+                        className="outDesign"
+                        onClick={() => dispatch(logout({ credentials: "" }))}
+                    >
+                        log out
+                    </div>
+                </div>
+            ) : (
+                <div className="navigatorDesign">
+                    <CLink path="/login" title="Login" />
+                    <CLink path="/register" title="Register" />
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
