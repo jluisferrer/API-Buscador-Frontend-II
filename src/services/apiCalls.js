b387c218688376d9b3c0f80041d34d7ed2b20a29
jluisferrer <jluisferrermartinez@gmail.com>
2024-04-05 15:27:45 +0200

    feat:add registeruser in apicalls

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -20,3 +20,24 @@ export const LoginUser = async (user) => {
         return error
     }
 }
+
+export const RegisterUser = async (user) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    }
+    try {
+        const response = await fetch(`${root}auth/register`, options)
+        const data = await response.json()
+
+        if (!data.success) {
+            throw new Error(data.message)
+        }
+        return data;
+    } catch (error) {
+        return error
+    }
+}
